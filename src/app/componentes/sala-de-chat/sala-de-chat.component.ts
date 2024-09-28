6b28dddb1674bb9aa0b2d8855ef9a3698c7c721e
tomasperezponisio <elunicotomas@gmail.com>
2024-09-28 10:16:37 -0300

    fix in chatroom autoscroll

--- src/app/componentes/sala-de-chat/sala-de-chat.component.ts
+++ src/app/componentes/sala-de-chat/sala-de-chat.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {addDoc, collection, Firestore} from "@angular/fire/firestore";
 import {Auth} from "@angular/fire/auth";
@@ -19,10 +19,14 @@ import {DatePipe, NgClass, NgForOf} from "@angular/common";
   templateUrl: './sala-de-chat.component.html',
   styleUrl: './sala-de-chat.component.less'
 })
-export class SalaDeChatComponent implements OnInit, OnDestroy {
+export class SalaDeChatComponent implements OnInit, OnDestroy, AfterViewChecked {
   public mensaje!: string;
-  public mensajesCollection:any[] = [];
-  private sub!:Subscription;
+  public mensajesCollection: any[] = [];
+  private sub!: Subscription;
+  private shouldScroll: boolean = false;
+
+  // Referencia al div de los chats para el autoscroll
+  @ViewChild('chatMessages') private chatMessagesContainer!: ElementRef;
 
   constructor(
     public auth: Auth,
@@ -31,14 +35,39 @@ export class SalaDeChatComponent implements OnInit, OnDestroy {
   ) {
   }
 
+  /**
+   * Me subscribo al metodo `traerMensajes` de `salaDeChatService`.
+   * Actualizo `mensajesCollection` con los mensajes recibidos y seteo `shouldScroll` a true.
+   *
+   * @return {void}
+   */
   ngOnInit(): void {
-    // Subscribe to the service to get real-time updates
     this.sub = this.salaDeChatService.traerMensajes().subscribe((mensajes) => {
       this.mensajesCollection = mensajes;
+      this.shouldScroll = true;
     });
   }
 
-  // Function to send a message
+  /**
+   *
+   * Si `shouldScroll` es true, llamo al metodo `scrollToBottom` para scrollear al fondo automaticamente,
+   * después reseteo el flag `shouldScroll` a false.
+   *
+   * @return {void}
+   */
+  ngAfterViewChecked(): void {
+    if (this.shouldScroll) {
+      this.scrollToBottom();
+      this.shouldScroll = false; // Reset the flag after scrolling
+    }
+  }
+
+  /**
+   * Envia un mensaje a la colección "sala-de-chat" en Firestore.
+   * Despues de enviar el mensaje resetea el input y scrollea al fondo.
+   *
+   * @return {void}
+   */
   public enviarMensaje() {
     if (this.mensaje.trim()) {
       const col = collection(this.firestore, 'sala-de-chat');
@@ -48,20 +77,30 @@ export class SalaDeChatComponent implements OnInit, OnDestroy {
         mensaje: this.mensaje
       });
       this.mensaje = '';
+      this.shouldScroll = true; // Ensure we scroll to the bottom after sending a message
     }
   }
 
-  private showSuccessAlert(message: string) {
-    return Swal.fire({
-      title: 'Sala de Chat',
-      text: message,
-      icon: 'success',
-      confirmButtonText: 'OK'
-    });
+  /**
+   * Scrollea al fondo el contenedor de los mensajes.
+   * Utiliza la referencia del div #chatMessages
+   *
+   * @return {void}
+   */
+  private scrollToBottom(): void {
+    try {
+      this.chatMessagesContainer.nativeElement.scrollTop = this.chatMessagesContainer.nativeElement.scrollHeight;
+    } catch (err) {
+      console.error('Error scrolling to the bottom: ', err);
+    }
   }
 
+  /**
+   * Me desubscribo al servicio de mensajes.
+   *
+   * @return {void} Does not return a value.
+   */
   ngOnDestroy(): void {
-    // Unsubscribe when the component is destroyed to prevent memory leaks
     if (this.sub) {
       this.sub.unsubscribe();
     }
