1e191c30e5e3ad9307c396dfffed7d96c410a505
tomasperezponisio <elunicotomas@gmail.com>
2024-10-07 22:25:13 -0300

    fix/ahorcado

--- src/app/modules/juegos/componentes/ahorcado/ahorcado.component.ts
+++ src/app/modules/juegos/componentes/ahorcado/ahorcado.component.ts
@@ -9,18 +9,18 @@ import Swal from "sweetalert2";
   styleUrl: './ahorcado.component.less'
 })
 export class AhorcadoComponent {
-  imrUrlBase: string = 'https://firebasestorage.googleapis.com/v0/b/tp1-sala-de-juegos.appspot.com/o';
-  palabra: string = '';  // The random word to guess
-  letrasUsadas: string[] = [];  // Letters the player has guessed
-  palabraOculta: string[] = [];  // Word display with underscores
-  maximoDeIntentos: number = 6;  // Max incorrect guesses before game over
-  errores: number = 0;  // Count incorrect guesses
-  gameOver: boolean = false;  // Tracks if the game is over
-  mensaje: string = '';  // Game over message
-  juegoIniciado: boolean = false;  // Track whether the game has started
-  puntaje: number = 0;  // Track the player's score
+  imgUrlBase: string = 'https://firebasestorage.googleapis.com/v0/b/tp1-sala-de-juegos.appspot.com/o';
+  imgUrls: string [] = [];
+  palabra: string = '';
+  letrasUsadas: string[] = [];
+  palabraOculta: string[] = [];
+  maximoDeIntentos: number = 6;
+  errores: number = 0;
+  gameOver: boolean = false;
+  mensaje: string = '';
+  juegoIniciado: boolean = false;
+  puntaje: number = 0;
 
-  // Alphabet for the on-screen keyboard
   abecedario: string[] = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'.toLowerCase().split('');
 
   constructor(
@@ -28,28 +28,50 @@ export class AhorcadoComponent {
   ) {
   }
 
-  // Start a new game
-  comenzarJuego() {
+  // Cargo las imagenes del ahorcado en memoria
+  ngOnInit(): void {
+    this.imgUrls[0] = this.imgUrlBase + '/hangman01.jpg?alt=media&token=621ef5e3-3432-4371-9374-bb616a0c8fce';
+    this.imgUrls[1] = this.imgUrlBase + '/hangman02.jpg?alt=media&token=2918ef5d-ab69-49bf-aa26-ecfc272c3dbe';
+    this.imgUrls[2] = this.imgUrlBase + '/hangman03.jpg?alt=media&token=bceba62c-a3b6-48ee-993a-a949a3423b1c';
+    this.imgUrls[3] = this.imgUrlBase + '/hangman04.jpg?alt=media&token=e7283828-40c2-40c4-9f95-6d602b8e3c34';
+    this.imgUrls[4] = this.imgUrlBase + '/hangman05.jpg?alt=media&token=5b7743d2-ad9d-448c-8b39-3479ef0b528b';
+    this.imgUrls[5] = this.imgUrlBase + '/hangman06.jpg?alt=media&token=bc2f7c77-ff60-4bb3-a263-280d0083a706';
+    this.imgUrls[5] = this.imgUrlBase + '/hangman07.jpg?alt=media&token=9fe787ed-7def-486a-ae7a-184ea5e2f2ef';
+  }
+
+
+  /**
+   * Inicia un juego nuevo seleccionando una palabra aleatoria del array
+   * reseta las variables del juego, y setea en true a `juegoIniciado`
+   *
+   * @return {void}
+   */
+  comenzarJuego(): void {
     const indiceRandom = Math.floor(Math.random() * palabras.length);
-    this.palabra = palabras[indiceRandom].toLowerCase();  // Select random word from array
-    this.palabraOculta = Array(this.palabra.length).fill('_');  // Initialize display with underscores
+    this.palabra = palabras[indiceRandom].toLowerCase();
+    this.palabraOculta = Array(this.palabra.length).fill('_');
     this.letrasUsadas = [];
     this.errores = 0;
     this.gameOver = false;
     this.mensaje = '';
-    this.juegoIniciado = true;  // Set the game as started
+    this.juegoIniciado = true;
+    console.log(this.palabra);
   }
 
-  // Handle player's guess
-  adivinarLetra(letter: string) {
+  /**
+   * Maneja la lógica de adivinar una letra de la palabra secreta.
+   *
+   * @param letter The letter being guessed by the player.
+   * @return void
+   */
+  adivinarLetra(letter: string): void {
     if (this.gameOver || this.letrasUsadas.includes(letter)) {
-      return;  // Do nothing if game is over or letter already guessed
+      return;
     }
 
-    this.letrasUsadas.push(letter);  // Add guessed letter to the array
+    this.letrasUsadas.push(letter);
 
     if (this.palabra.includes(letter)) {
-      // Correct guess: update the displayWord
       for (let i = 0; i < this.palabra.length; i++) {
         if (this.palabra[i] === letter) {
           this.palabraOculta[i] = letter;
@@ -73,6 +95,7 @@ export class AhorcadoComponent {
           this.guardaPuntaje(this.puntaje, 'ahorcado');
           this.gameOver = true;
         });
+        this.errores = 0;
       }
     }
   }
@@ -81,7 +104,7 @@ export class AhorcadoComponent {
    * Reinicia el juego
    */
   reiniciarJuego() {
-    this.puntaje = 0;  // Reset the score
+    this.puntaje = 0;
     this.comenzarJuego();
   }
 
