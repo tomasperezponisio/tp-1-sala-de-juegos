25cd9041ea1d7d6d506ebf1dc29d6de23eb7dd5a
tomasperezponisio <elunicotomas@gmail.com>
2024-10-20 11:42:13 -0300

    fixes in juego-de-reflejos

--- src/app/modules/juegos/componentes/juego-de-reflejos/juego-de-reflejos.component.ts
+++ src/app/modules/juegos/componentes/juego-de-reflejos/juego-de-reflejos.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import Swal from 'sweetalert2'
 import {PuntajesService} from "../../../../services/puntajes.service";
 
@@ -8,43 +8,72 @@ import {PuntajesService} from "../../../../services/puntajes.service";
   styleUrl: './juego-de-reflejos.component.less'
 })
 
-export class JuegoDeReflejosComponent implements OnInit {
-  //TODO: fix start/end of the game
-  puntaje = 0;
-  tiempoRestante = 15;
-  gameOver = false;
-  mostrarBoton = false;
-  tamanioBoton = 80;
-  maxObstaculos = 120;
-  minObstaculos = 90;
+export class JuegoDeReflejosComponent implements OnInit, OnDestroy {
+  puntaje: number = 0;
+  tiempoRestante: number = 15;
+  gameOver: boolean = false;
+  juegoIniciado: boolean = false;
+  mostrarBoton: boolean = false;
+  tamanioBoton: number = 80;
+  maxObstaculos: number = 60;
+  minObstaculos: number = 60;
   mensaje: string = '';
-  public goodSmileyImg: string = 'https://firebasestorage.googleapis.com/v0/b/tp1-sala-de-juegos.appspot.com/o/good-smiley-cropped.png?alt=media&token=6a7fddef-2981-403b-8238-5c326916e3e4';
-  public badSmileyImg: string = 'https://firebasestorage.googleapis.com/v0/b/tp1-sala-de-juegos.appspot.com/o/bad-smiley-cropped.png?alt=media&token=84eee346-cc0e-4c03-99e3-241dfa1e7240';
+  goodSmileyImg: string = 'https://firebasestorage.googleapis.com/v0/b/tp1-sala-de-juegos.appspot.com/o/good-smiley-cropped.png?alt=media&token=6a7fddef-2981-403b-8238-5c326916e3e4';
+  badSmileyImg: string = 'https://firebasestorage.googleapis.com/v0/b/tp1-sala-de-juegos.appspot.com/o/bad-smiley-cropped.png?alt=media&token=84eee346-cc0e-4c03-99e3-241dfa1e7240';
 
   // Posición del botón correcto
-  posicionX = 0;
-  posicionY = 0;
+  posicionX: number = 0;
+  posicionY: number = 0;
 
   // Arreglo de obstáculos
-  obstaculos: {x: number, y: number, isVisible: boolean}[] = [];
+  obstaculos: { x: number, y: number, isVisible: boolean }[] = [];
 
   private timerInterval: any;
-  //private obstacleCount = 3;  // Número de obstáculos
+
+  // Area de juego
+  areaDeJuego: Element | null = null;
+  anchoAreaDeJuego?: number;
+  altoAreaDeJuego?: number;
 
   constructor(
     private puntajesService: PuntajesService
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
-    this.empezarJuego();
+    this.preCargaDeImagenes();
+
+    clearInterval(this.timerInterval);
+
+    this.areaDeJuego = document.querySelector('.game-area');
+    this.anchoAreaDeJuego = this.areaDeJuego?.clientWidth;
+    this.altoAreaDeJuego = this.areaDeJuego?.clientHeight;
+  }
+
+  // Preload the images
+  preCargaDeImagenes(): void {
+    const imagesToPreload = [
+      this.goodSmileyImg,
+      this.badSmileyImg
+    ];
+
+    imagesToPreload.forEach(src => {
+      const img = new Image();
+      img.src = src;
+    });
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.timerInterval);
   }
 
   empezarJuego(): void {
+    this.juegoIniciado = true;  // NEW: Mark the game as started
     this.reiniciarJuego();
     this.empezarTimer();
     this.moverBoton();
 
-    const randCantidadObstaculos = Math.floor(Math.random() * (this.maxObstaculos - this.minObstaculos + 1)) + this.minObstaculos;
+    const randCantidadObstaculos: number = Math.floor(Math.random() * (this.maxObstaculos - this.minObstaculos + 1)) + this.minObstaculos;
     this.generarObstaculos(randCantidadObstaculos);
   }
 
@@ -59,8 +88,8 @@ export class JuegoDeReflejosComponent implements OnInit {
 
   // Penalizar al jugador si hace clic en un obstáculo
   alClickearUnObstaculo(): void {
-    this.mensaje = 'Hiciste clic en un obstáculo. ¡Juego terminado!';
-    this.showErrorAlert(this.mensaje).then(() => {
+    this.mensaje = 'Juego terminado! Puntaje final: ' + this.puntaje;
+    this.showErrorAlert('Perdiste!', this.mensaje).then(() => {
       this.guardaPuntaje(this.puntaje, 'juego-de-reflejos');
       this.finalizarJuego();
     });
@@ -68,14 +97,12 @@ export class JuegoDeReflejosComponent implements OnInit {
 
   // Mover el botón a una posición aleatoria dentro del área de juego
   moverBoton(): void {
-    const areaDeJuego = document.querySelector('.container');
-    if (areaDeJuego) {
-      const width = areaDeJuego.clientWidth;
-      const height = areaDeJuego.clientHeight;
-
-      // Calcular posiciones aleatorias para el botón correcto
-      this.posicionX = Math.floor(Math.random() * (width - (this.tamanioBoton * 2))); // Restar 100 para evitar que salga del contenedor
-      this.posicionY = Math.floor(Math.random() * (height - (this.tamanioBoton * 2)));
+    if (this.areaDeJuego) {
+      if (this.anchoAreaDeJuego && this.altoAreaDeJuego ) {
+        // Calcular posiciones aleatorias para el botón correcto
+        this.posicionX = Math.floor(Math.random() * (this.anchoAreaDeJuego - (this.tamanioBoton * 2))); // Restar 100 para evitar que salga del contenedor
+        this.posicionY = Math.floor(Math.random() * (this.altoAreaDeJuego - (this.tamanioBoton * 2)));
+      }
     }
     this.mostrarBoton = true;
   }
@@ -84,39 +111,37 @@ export class JuegoDeReflejosComponent implements OnInit {
   generarObstaculos(cantidadObstaculos: number): void {
     this.obstaculos = [];  // Reiniciar obstáculos
 
-    const areaDeJuego = document.querySelector('.container');
     const distanciaMinima = this.tamanioBoton * 0.5; // Minimum distance is 30% of the button's size
 
-    if (areaDeJuego) {
-      const width = areaDeJuego.clientWidth;
-      const height = areaDeJuego.clientHeight;
+    if (this.areaDeJuego) {
+      if (this.anchoAreaDeJuego && this.altoAreaDeJuego ) {
+        for (let i = 0; i < cantidadObstaculos; i++) {
+          let x: number;
+          let y: number;
+          let esPosicionValida: boolean;
 
-      for (let i = 0; i < cantidadObstaculos; i++) {
-        let x: number;
-        let y: number;
-        let esPosicionValida: boolean;
+          // Genera posiciones hasta que una es valida
+          do {
+            x = Math.floor(Math.random() * (this.anchoAreaDeJuego - (this.tamanioBoton)));
+            y = Math.floor(Math.random() * (this.altoAreaDeJuego - (this.tamanioBoton)));
+            esPosicionValida = true;
 
-        // Keep generating positions until a valid one is found
-        do {
-          x = Math.floor(Math.random() * (width - (this.tamanioBoton * 2)));
-          y = Math.floor(Math.random() * (height - (this.tamanioBoton * 2)));
-          esPosicionValida = true;
-
-          // Check against the position of the main button
-          if (Math.abs(x - this.posicionX) < distanciaMinima && Math.abs(y - this.posicionY) < distanciaMinima) {
-            esPosicionValida = false;
-          }
-
-          // Check against the positions of existing obstacles
-          for (const obstacle of this.obstaculos) {
-            if (Math.abs(x - obstacle.x) < distanciaMinima && Math.abs(y - obstacle.y) < distanciaMinima) {
+            // Valida contra la posicion del boton "bueno"
+            if (Math.abs(x - this.posicionX) < distanciaMinima && Math.abs(y - this.posicionY) < distanciaMinima) {
               esPosicionValida = false;
-              break;
             }
-          }
-        } while (!esPosicionValida);
 
-        this.obstaculos.push({ x, y, isVisible: true });
+            // Valida contra las posiciones de los demas obstaculos
+            for (const obstacle of this.obstaculos) {
+              if (Math.abs(x - obstacle.x) < distanciaMinima && Math.abs(y - obstacle.y) < distanciaMinima) {
+                esPosicionValida = false;
+                break;
+              }
+            }
+          } while (!esPosicionValida);
+
+          this.obstaculos.push({x, y, isVisible: true});
+        }
       }
     }
   }
@@ -130,8 +155,8 @@ export class JuegoDeReflejosComponent implements OnInit {
     this.timerInterval = setInterval(() => {
       this.tiempoRestante--;
       if (this.tiempoRestante <= 0) {
-        this.mensaje = 'Se acabo el tiempo! Juego terminado!';
-        this.showErrorAlert(this.mensaje).then(() => {
+        this.mensaje = 'Juego terminado! Puntaje final: ' + this.puntaje;
+        this.showErrorAlert('Se acabó el tiempo!', this.mensaje).then(() => {
           this.guardaPuntaje(this.puntaje, 'juego-de-reflejos');
           this.finalizarJuego();
         });
@@ -150,9 +175,8 @@ export class JuegoDeReflejosComponent implements OnInit {
   // Reiniciar el juego
   reiniciarJuego(): void {
     this.puntaje = 0;
-    this.tiempoRestante = 30;
+    //this.tiempoRestante = 15;
     this.gameOver = false;
-    clearInterval(this.timerInterval);
   }
 
   /**
@@ -169,12 +193,13 @@ export class JuegoDeReflejosComponent implements OnInit {
 
   /**
    * Muestra mensaje de error
+   * @param titulo
    * @param message
    * @private
    */
-  private showErrorAlert(message: string) {
+  private showErrorAlert(titulo: string, message: string) {
     return Swal.fire({
-      title: 'Perdiste!',
+      title: titulo,
       text: message,
       icon: 'error',
       confirmButtonText: 'Cerrar'
